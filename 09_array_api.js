c8a9a2a62075804a60ba9d7409b38d9b968f514f
seojeong89 <halim1378@gmail.com>
2020-10-28 10:51:48 +0900

    20/10/28

--- 09_array_api.js
+++ 09_array_api.js
@@ -44,54 +44,94 @@ console.log(books)
 // 소주
 
 // const sojus = [
-//     { name: '대선', price: 1200, sale: true, score: 86 },
-//     { name: '좋은데이', price: 1100, sale: false, score: 92 },
-//     { name: '진로', price: 1150, sale: true, score: 80 },
-//     { name: '금복주', price: 980, sale: false, score: 60 },
-//     { name: '참이슬', price: 1250, sale: true, score: 46 }
-//   ]
+//   { name: '대선', price: 1200, sale: true, score: 86 },
+//   { name: '좋은데이', price: 1100, sale: false, score: 92 },
+//   { name: '진로', price: 1150, sale: true, score: 80 },
+//   { name: '금복주', price: 980, sale: false, score: 60 },
+//   { name: '참이슬', price: 1250, sale: true, score: 46 }
+// ]
 
-//   class Soju {
-//     constructor(name, price, sale, score) {
-//       this.name = name
-//       this.price = price
-//       this.sale = sale
-//       this.score = score
-//     }
-//   }
+class Soju {
+  constructor(name, price, sale, score) {
+    this.name = name
+    this.price = price
+    this.sale = sale
+    this.score = score
+  }
+}
 
-//   const sojus = [
-//     new Soju('대선', '1200', 'true', '86')
-//   new Soju('좋은데이', '1100', 'false', '92')
-//   new Soju('진로', '1150', 'true', '80')
-//   new Soju('금복주', '980', 'false', '60')
-//   new Soju('참이슬', '1250', 'true', '46')
-//   ]
+const sojus = [
+  new Soju('대선', 1200, true, 86),
+  new Soju('좋은데이', 1100, false, 92),
+  new Soju('진로', 1150, true, 80),
+  new Soju('금복주', 980, false, 60),
+  new Soju('참이슬', 1250, true, 46)
+]
 
 console.clear()
 
 // 5. score 80인 소주 찾기
 {
-  const sojus = [
-    { name: '대선', price: 1200, sale: true, score: 86 },
-    { name: '좋은데이', price: 1100, sale: false, score: 92 },
-    { name: '진로', price: 1150, sale: true, score: 80 },
-    { name: '금복주', price: 980, sale: false, score: 60 },
-    { name: '참이슬', price: 1250, sale: true, score: 46 }
-  ]
-
+  const result = sojus.find(soju => soju.score === 80)
+  console.log(result)
 }
 
-
-
 // 6. 판매중인 소주를 찾아서 새로운 배열로 만들기
+{
+  const result = sojus.filter(item => item.sale)
+  console.log(result)
+}
+{
+  const result = sojus.filter(item => !item.sale)
+  console.log(result)
+}
 
 // 7. score만 있는 새로운 배열 만들기
+{
+  const result = sojus.map(soju => soju.score)
+  console.log(result)
+}
 
 // 8. score가 50보다 작은 소주가 있는지 확인하기
+{
+  const result = sojus.some(soju => soju.score < 50)
+  console.log(result)
+}
 
 // 9. 소주들의 평균 가격을 계산하기
+{
+  const result = sojus.reduce((a, b) => {
+    return a + b.price
+  }, 0)
+  console.log(result / sojus.length)
+}
 
 // 10. 점수를 문자로 만들기
+{
+  const result = sojus
+    .map(soju => soju.score)
+    .join('/')
+  console.log(result)
+}
 
 // 11. 점수가 낮은 순으로, 문자열로 배열하기
+{
+  const result = sojus
+    .map(soju => soju.score)
+    .sort((a, b) => a - b)
+    .join()
+
+  console.log(result)
+}
+
+{
+  const result = sojus
+    .map(soju => soju.score)
+    .sort((a, b) => b - a)
+    .join()
+
+  console.log(result)
+}
+
+// 비동기 통신 -> callback, primise, async await
+// 클로즈
--- app.js
+++ app.js
@@ -14,11 +14,16 @@
 // })
 
 // 구구단
-for (let i = 2; i < 10; i++) {
-  console.log(`${i}단`)
-  console.log('-----------------')
-  for (let j = 1; j < 10; j++) {
-    console.log(`${i}x${j}=${i * j}`)
+{
+  for (let i = 2; i < 10; i++) {
+    console.log(`${i}단`)
+    console.log('-----------------')
+    for (let j = 1; j < 10; j++) {
+      console.log(`${i}x${j}=${i * j}`)
+    }
+    console.log('-----------------')
   }
-  console.log('-----------------')
 }
+
+console.clear()
+
